7b5b80bd9ee70ac92ed1842127a58e00daff4008
ace <andrew.emil@gmail.com>
2018-02-24 16:24:49 -0500

    Moved config to example

--- functions/config_example.js
+++ functions/config_example.js
@@ -4,7 +4,7 @@
  * @param {!Object} req Cloud Function request context.
  * @param {!Object} res Cloud Function response context.
  */
-exports.getConfig = () => {
+exports.getConfigExample = () => {
   return   {account_sid: "account_sid",
             auth_token: "auth_token", 
             my_phone: "+123456789",
